dc1220592484363d8709baba809f93fd116f470a
Numan Ahmed <mdnmnahmed@gmail.com>
2023-12-09 08:15:20 +0530

    route specific not-found page

--- src/app/products/[productId]/reviews/[reviewId]/page.tsx
+++ src/app/products/[productId]/reviews/[reviewId]/page.tsx
@@ -1,4 +1,9 @@
+import { notFound } from "next/navigation"
+
 export default function ReviewDetails({ params }: { params: { reviewId: string, productId: string } }) {
+    if (parseInt(params.reviewId) > 100) {
+        notFound();
+    }
     return (
         <div>
             <h1>Review Details Page  </h1>
