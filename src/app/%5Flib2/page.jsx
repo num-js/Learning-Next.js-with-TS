9746fbada7eeaabd111f12fa2355b1d4578ce237
Numan Ahmed <mdnmnahmed@gmail.com>
2023-12-09 08:29:09 +0530

    private folder & '_' underscore in URL

---
+++ src/app/%5Flib2/page.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function PrivatePage() {
+    return (
+        <div>
+            <h1>Page with underscore</h1>
+            <p>to use '_' underscore in URL, Folder naming conviction should be '%5F', For Example - %5Flib</p>
+        </div>
+    )
+}
