269062fa036a36bcd190977e19b03f58e1df8efd
Numan Ahmed <mdnmnahmed@gmail.com>
2023-12-12 22:12:17 +0530

    deep dive in title with string & object

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -1,7 +1,15 @@
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+    title: {
+        absolute: "Blog"
+    }
+}
+
 export default function Blog() {
-  return (
-    <div>
-        <h1>Blog Page</h1>
-    </div>
-  )
+    return (
+        <div>
+            <h1>Blog Page</h1>
+        </div>
+    )
 }
