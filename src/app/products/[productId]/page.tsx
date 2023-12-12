909c80dc0766af94c774f988ead596ac71307b9f
Numan Ahmed <mdnmnahmed@gmail.com>
2023-12-12 20:56:06 +0530

    dynamic meta-data from page with 'generateMetadata' function

--- src/app/products/[productId]/page.tsx
+++ src/app/products/[productId]/page.tsx
@@ -1,7 +1,37 @@
-export default function ProductDetails({params}: {params: {productId: string}}) {
-  return (
-    <>
-        <h1>Product Details of - {params.productId}</h1>
-    </>
-  )
+import { Metadata } from "next";
+
+type Props = {
+    params: {
+        productId: string
+    }
+}
+
+
+//generateMetadata is a Next.js function for change title or metadata.
+/* export const generateMetadata = ({ params }: Props): Metadata => {
+  return {
+    title: `Product - ${params.productId}`
+  }
+} */
+
+
+// We can use Async for MetaData, if some scenarios need to fetch title from API
+export const generateMetadata = async ({ params }: Props): Promise<Metadata> => {
+    const title = await new Promise((resolve) => {
+        setTimeout(() => {
+            resolve(`${params.productId}`);
+        }, 500); // This will increase Page-load, page will not render until it resolved
+    });
+    return {
+        title: `response Product - ${title}`
+    }
+}
+
+
+export default function ProductDetails({ params }: Props) {
+    return (
+        <>
+            <h1>Product Details of - {params.productId}</h1>
+        </>
+    )
 }
