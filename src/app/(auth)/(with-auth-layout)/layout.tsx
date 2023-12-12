2feff3d51860197065bf4371580d847ca0c6ce6a
Numan Ahmed <mdnmnahmed@gmail.com>
2023-12-12 14:47:53 +0530

    auth-layout nested layout with route-group

---
+++ src/app/(auth)/(with-auth-layout)/layout.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function AuthLayout({ children }: { children: React.ReactNode }) {
+    return (
+        <>
+            <h1>Auth Layout</h1>
+            {children}
+        </>
+    )
+}
