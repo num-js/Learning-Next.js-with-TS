d74ad1628f957440e67ac81b6d8115b2ad34e123
Numan Ahmed <mdnmnahmed@gmail.com>
2023-12-12 22:34:14 +0530

    dynamic route navigation

--- src/app/products/page.tsx
+++ src/app/products/page.tsx
@@ -1,10 +1,19 @@
+import Link from "next/link";
+
 export default function ProductList() {
+
+  const someProductId = "realme-phone";
+
   return (
     <>
-        <h1>Product List --- </h1>
-        <p>Product 1</p>
-        <p>Product 2</p>
-        <p>Product 3</p>
+      <h1>Product List --- </h1>
+      <p><Link href="products/1">Product 1</Link></p>
+      <p><Link href="products/2">Product 2</Link></p>
+      <p><Link href="products/3" replace>Product 3</Link></p>
+      {/* replace - prop to Link component, replaces browser history */}
+
+      <p>Dynamic Product - Routing</p>
+      <p><Link href={`products/${someProductId}`}>Some Product Id - {someProductId}</Link></p>
     </>
   )
 }
