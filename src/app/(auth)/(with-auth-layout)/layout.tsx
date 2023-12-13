4abcc99e909f99badca7e4dfbb5ff25ad9e06fb2
Numan Ahmed <mdnmnahmed@gmail.com>
2023-12-13 07:10:37 +0530

    Navigation active link

--- src/app/(auth)/(with-auth-layout)/layout.tsx
+++ src/app/(auth)/(with-auth-layout)/layout.tsx
@@ -1,9 +1,33 @@
+"use client";
+import Link from "next/link";
 import React from "react";
+import { usePathname } from 'next/navigation'
+
+const navLinks = [
+    { name: "Register", link: "/register" },
+    { name: "Login", link: "/login" },
+    { name: "Forgot Password", link: "/forgot-password" },
+];
 
 export default function AuthLayout({ children }: { children: React.ReactNode }) {
+    const pathName = usePathname();
     return (
         <>
             <h1>Auth Layout</h1>
+            {
+                navLinks.map(navLink => {
+                    const isActiveLink = pathName.startsWith(navLink.link);
+                    return (
+                        <Link
+                            key={navLink.name}
+                            href={navLink.link}
+                            className={`mr-4 ${isActiveLink ? "font-bold" : "text-blue-500"}`}
+                        >
+                            {navLink.name}
+                        </Link>
+                    )
+                })
+            }
             {children}
         </>
     )
