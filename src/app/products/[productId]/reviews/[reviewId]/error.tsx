e4a3f4584a3e2eb02c7751e88ea1f11fdd217c46
Numan Ahmed <mdnmnahmed@gmail.com>
2023-12-19 06:48:29 +0530

    Next.js-14 'Try Again' button for reset & re-render 'page.tsx'

--- src/app/products/[productId]/reviews/[reviewId]/error.tsx
+++ src/app/products/[productId]/reviews/[reviewId]/error.tsx
@@ -1,9 +1,14 @@
 "use client";
 
-export default function ErrorBoundary({ error }: { error: Error }) {
+export default function ErrorBoundary({ error, reset }: { error: Error; reset: () => void; }) {
     return (
-        <div className="text-center font-bold text-red-600">
-            {error.message}
-        </div>
+        <>
+            <div className="text-center font-bold text-red-600">
+                {error.message}
+            </div>
+            <div className="text-center font-bold">
+                <button onClick={reset} className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">Try Again</button>
+            </div>
+        </>
     )
 }
--- src/app/products/[productId]/reviews/[reviewId]/page.tsx
+++ src/app/products/[productId]/reviews/[reviewId]/page.tsx
@@ -1,3 +1,4 @@
+"use client";
 import { notFound } from "next/navigation"
 
 const getRandomNumber = (count: number) => {
